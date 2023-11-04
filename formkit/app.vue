8769f27059d69432dca53aeb3a17a30088b53053
Brian DeShong <brian@deshong.net>
2023-11-03 21:13:35 -0400

    WIP.

--- formkit/app.vue
+++ formkit/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <FormKit type="text" label="Username" name="username" help="Pick a username" validation="required|matches:/^@[a-zA-Z]+$/|length:5" value="@useFormKit" prefix-icon="avatarMan"/>
+    <FormKit type="select" />
+    <FormKit type="textarea" />
   </div>
 </template>
--- formkit/formkit.config.ts
+++ formkit/formkit.config.ts
@@ -1,7 +1,7 @@
 import { DefaultConfigOptions } from "@formkit/vue";
 
 const config: DefaultConfigOptions = {
-    theme: "genesis"
+    theme: "genesis",
 }
 
 export default config;
