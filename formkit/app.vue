2064be7b9690db6b993e35a4ef1df1023f121254
Brian DeShong <brian@deshong.net>
2023-11-03 21:38:55 -0400

    WIP.

--- formkit/app.vue
+++ formkit/app.vue
@@ -14,10 +14,14 @@ async function handleSubmit(data) {
 </script>
 <template>
   <div>
-    <FormKit type="form" :value="formData" @submit="handleSubmit">
-      <h1>Login</h1>
-      <FormKit type="text" label="Username" name="username" />
-      <FormKit type="password" label="Password" name="password" />
+    <FormKit type="form" :value="formData" @submit="handleSubmit" submit-behavior="live" submit-label="Login" :actions="false">
+      <template #default="{ state }">
+        <h1>Login</h1>
+        <FormKit type="text" label="Username" name="username" />
+        <FormKit type="password" label="Password" name="password" />
+        <button :disabled="state.loading">
+        {{ state.loading ? 'Loading...' : 'Login' }}</button>
+      </template>
     </FormKit>
   </div>
 </template>
