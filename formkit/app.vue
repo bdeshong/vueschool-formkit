0d54d7d73771c91fdc164fd3cb8b4227df0dc140
Brian DeShong <brian@deshong.net>
2023-11-03 21:26:05 -0400

    WIP.

--- formkit/app.vue
+++ formkit/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <FormKit type="text" label="Username" name="username" help="Pick a username" validation="required|matches:/^@[a-zA-Z]+$/|length:5" value="@useFormKit" prefix-icon="avatarMan"/>
+    <FormKit type="text" label="Username" name="username" help="Pick a username" validation="required|matches:/^@[a-zA-Z]+$/|length:5" value="@useFormKit" prefix-icon="avatarMan" inner-class="mycustom-inner">
+      <template #label="context">
+        {{ context.label }}
+        ? <span>{{ context.help}}</span>
+      </template>
+      <template #help>
+      </template>
+    </FormKit>
     <FormKit type="select" />
     <FormKit type="textarea" />
   </div>
