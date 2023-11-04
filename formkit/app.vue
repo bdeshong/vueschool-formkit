1fee798efcf1b622de7faaa8bf38a3960b897fd1
Brian DeShong <brian@deshong.net>
2023-11-03 22:00:07 -0400

    WIP.

--- formkit/app.vue
+++ formkit/app.vue
@@ -22,7 +22,7 @@ async function username_is_unique(node) {
     <FormKit type="form" :value="formData" @submit="handleSubmit" submit-label="Login">
       <template #default="{ state }">
         <h1>Login</h1>
-        <FormKit type="text" label="Username" name="username" validation="(500)username_is_unique" />
+        <FormKit type="text" label="Username" name="username" validation="(500)username_is_unique"  :validation-messages="{ username_is_unique: 'That username is already taken.'}"/>
         <FormKit type="password" label="Password" name="password" />
       </template>
     </FormKit>
